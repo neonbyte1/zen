1a8b85a6b647fafdf0b9403157f779bd2021b4f8
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2025-03-08 14:53:02 +0100

    🏷️ feat(platform): added main_reason type

--- include/zen/platform/constants.hpp
+++ include/zen/platform/constants.hpp
@@ -27,6 +27,46 @@
 #include <zen/core/requirements.hpp>
 
 namespace zen::win {
+enum struct main_reason : u32
+{
+    // Alias for DLL_PROCESS_DETACH
+    //
+    // The DLL is being unloaded from the virtual address space of the calling process because it was loaded
+    // unsuccessfully or the reference count has reached zero (the processes has either terminated or called
+    // FreeLibrary one time for each time it called LoadLibrary). The lpvReserved parameter indicates whether
+    // the DLL is being unloaded as a result of a FreeLibrary call, a failure to load, or process termination.
+    // The DLL can use this opportunity to call the TlsFree function to free any TLS indices allocated by
+    // using TlsAlloc and to free any thread local data. Note that the thread that receives the process_detach
+    // notification is not necessarily the same thread that received the process_attach notification.
+    process_detach = 0,
+
+    // Alias for DLL_PROCESS_ATTACH
+    //
+    // The DLL is being loaded into the virtual address space of the current process as a result of the process
+    // starting up or as a result of a call to LoadLibrary. DLLs can use this opportunity to initialize any
+    // instance data or to use the TlsAlloc function to allocate a thread local storage (TLS) index.
+    // The lpvReserved parameter indicates whether the DLL is being loaded statically or dynamically.
+    process_attach,
+
+    // Alias for DLL_THREAD_ATTACH
+    //
+    // The current process is creating a new thread. When this occurs, the system calls the entry-point function
+    // of all DLLs currently attached to the process. The call is made in the context of the new thread.
+    // DLLs can use this opportunity to initialize a TLS slot for the thread. A thread calling the DLL entry-point
+    // function with process_attach does not call the DLL entry-point function with thread_attach. Note that
+    // a DLL's entry-point function is called with this value only by threads created after the DLL is loaded
+    // by the process. When a DLL is loaded using LoadLibrary, existing threads do not call the entry-point
+    // function of the newly loaded DLL.
+    thread_attach,
+
+    // Alias for DLL_THREAD_DETACH
+    //
+    // A thread is exiting cleanly. If the DLL has stored a pointer to allocated memory in a TLS slot, it should
+    // use this opportunity to free the memory. The system calls the entry-point function of all currently loaded
+    // DLLs with this value. The call is made in the context of the exiting thread.
+    thread_detach
+};
+
 enum struct allocation_type : u32
 {
     extended_parameter_numa_node_mandatory      = 0x00000000,
