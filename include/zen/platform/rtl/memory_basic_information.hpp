df6cc871a87bc49f92dbc7151f6aa4d09cc26140
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2025-07-06 23:25:28 +0200

    🎨 refactor(platform): using enum types instead

--- include/zen/platform/rtl/memory_basic_information.hpp
+++ include/zen/platform/rtl/memory_basic_information.hpp
@@ -40,27 +40,47 @@ enum struct memory_info : u32
 
 struct memory_basic_information32
 {
-    u32 base_address{};
-    u32 allocation_base{};
-    u32 allocation_protect{};
-    u32 region_size{};
-    u32 state{};
-    u32 protect{};
-    u32 type{};
+    u32                  base_address{};
+    u32                  allocation_base{};
+    u32                  allocation_protect{};
+    u32                  region_size{};
+    win::allocation_type state{};
+    win::page_protection protect{};
+    u32                  type{};
+
+    NODISCARD
+    constexpr
+    auto
+    has_protection(
+        const win::page_protection mask
+    ) const noexcept -> bool
+    {
+        return !!(std::to_underlying(protect) & std::to_underlying(mask));
+    }
 };
 
 struct ZEN_DECLSPEC_ALIGN(16) memory_basic_information64
 {
-    u64 base_address{};
-    u64 allocation_base{};
-    u32 allocation_protect{};
-    u16 partition_id{};
-    u16 alignment1{};
-    u64 region_size{};
-    u32 state{};
-    u32 protect{};
-    u32 type{};
-    u32 alignment2{};
+    u64                  base_address{};
+    u64                  allocation_base{};
+    u32                  allocation_protect{};
+    u16                  partition_id{};
+    u16                  alignment1{};
+    u64                  region_size{};
+    win::allocation_type state{};
+    win::page_protection protect{};
+    u32                  type{};
+    u32                  alignment2{};
+
+    NODISCARD
+    constexpr
+    auto
+    has_protection(
+        const win::page_protection mask
+    ) const noexcept -> bool
+    {
+        return !!(std::to_underlying(protect) & std::to_underlying(mask));
+    }
 };
 
 template<bool X64 = true>
